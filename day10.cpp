bbeeb2c1fb63596c511530f594c53ac71f9c644f
RohitRana76 <86055316+RohitRana76@users.noreply.github.com>
2021-07-29 20:24:14 +0530

    day10.cpp

---
+++ day10.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int threeSweatersCost = 204, compGameCst = 75,twoBracltCst=86, refundCst=43, rebateCst = 10;
+    int totalCost = threeSweatersCost+compGameCst+twoBracltCst-refundCst+rebateCst;
+
+    cout<<"The total amount consumer has to pay for the gifts are : "<<totalCost<<"$";
+    return 0;
+}
