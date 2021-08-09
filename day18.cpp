2528fd2deab5333142e6bb84fa13b64495f36269
RohitRana76 <86055316+RohitRana76@users.noreply.github.com>
2021-08-09 20:31:27 +0530

    day18.cpp

---
+++ day18.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "enter the number to find its square" << endl;
+    cin >> num;
+
+    // int squareOfNumber = pow(num,2);
+    cout << "the square of a number is : " <<  pow(num,2) << endl;
+    
+    return 0;
+}
