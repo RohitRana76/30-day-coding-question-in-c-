7198d37d2e20e02ea1a5dd4179465023e8e30483
RohitRana76 <86055316+RohitRana76@users.noreply.github.com>
2021-08-21 20:29:26 +0530

    day29.cpp

---
+++ day29.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printAll(string a[], int n)
+{
+    for(int i=0; i<n; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+void allCombination(string a[], int n)
+{
+    sort(a, a+n);
+    cout << "Possible combination:\n";
+    do{
+        printAll(a,n);
+    }while(next_permutation(a, a+n));
+}
+
+int main()
+{
+    string studentArray[] = {"Ram", "Anuj","Deepak","Ravi"};
+
+    int n = sizeof(studentArray)/sizeof(studentArray[0]);
+
+    allCombination(studentArray, n);
+
+    return 0;
+}
